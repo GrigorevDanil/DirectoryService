d58afa9c1576c5aa124b353225fd89b1d79b7b32
GrigorevDanil <danilg467@gmail.com>
2025-10-20 15:29:09 +0300

    add rollback for lockDescending

--- DirectoryService/src/DirectoryService.Application/Departments/UseCases/Move/MoveDepartmentHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/UseCases/Move/MoveDepartmentHandler.cs
@@ -67,7 +67,14 @@ public class MoveDepartmentHandler : ICommandHandler<MoveDepartmentCommand, Guid
         var lockDescendingResult = await _departmentRepository.LockDescending(department.Path, cancellationToken);
 
         if (lockDescendingResult.IsFailure)
+        {
+            var rollbackResult = transactionScope.Rollback();
+
+            if (rollbackResult.IsFailure)
+                return rollbackResult.Error.ToErrors();
+
             return lockDescendingResult.Error.ToErrors();
+        }
 
         // Присоединение подразделения к родителю
         if (command.Request.ParentId != null)
