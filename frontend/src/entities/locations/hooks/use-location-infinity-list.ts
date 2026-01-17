e52c76ab09d11264b9a20dea4b13cadac9d85991
GrigorevDanil <danilg467@gmail.com>
2026-01-17 18:44:47 +0300

    add filters for location list

--- frontend/src/entities/locations/hooks/use-location-infinity-list.ts
+++ frontend/src/entities/locations/hooks/use-location-infinity-list.ts
@@ -1,7 +1,24 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { locationsApi } from "../api";
+import {
+  useLocationActive,
+  useLocationPageSize,
+  useLocationSearch,
+  useLocationSortBy,
+  useLocationSortDirection,
+} from "../model/location-list-store";
+import { useDebounce } from "use-debounce";
 
 export const useLocationInfinityList = () => {
+  const search = useLocationSearch();
+  const [debouncedSearch] = useDebounce(search, 300);
+  const activeState = useLocationActive();
+  const sortBy = useLocationSortBy();
+  const sortDirection = useLocationSortDirection();
+  const pageSize = useLocationPageSize();
+
+  const isActive = activeState === "all" ? undefined : activeState === "active";
+
   const {
     data,
     isPending,
@@ -12,7 +29,13 @@ export const useLocationInfinityList = () => {
     isFetching,
     isFetchingNextPage,
   } = useInfiniteQuery({
-    ...locationsApi.getLocationsInfinityQueryOptions({}),
+    ...locationsApi.getLocationsInfinityQueryOptions({
+      search: debouncedSearch,
+      isActive,
+      sortBy,
+      sortDirection,
+      pageSize,
+    }),
   });
 
   return {
